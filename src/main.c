3ffef486af1d68b385c3d4ea9df3fd32f6fbbf21
Lu-Luou <rubertolh07@gmail.com>
2025-12-11 21:38:21 -0300

    pude compilar

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, World!!!\n");
     return 0;
 }
+
+// cd /c/Users/ruber/source/repos/vulnscanner/build
+// cmake ..
+// mingw32-make
+// ./vulnscanner.exe
